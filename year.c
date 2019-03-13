acf51fb6b002d0f2cd9ca5489b22de92dd5289b2
klHaritha <48052673+klHaritha@users.noreply.github.com>
2019-03-13 03:23:15 -0700

    Create year.c

---
+++ year.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("enter year");
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+        printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+
+}
