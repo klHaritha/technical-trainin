434bf6d65a3c724db154e31f73ab0c67d401456e
klHaritha <48052673+klHaritha@users.noreply.github.com>
2019-03-13 03:43:28 -0700

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+
+    printf("enter character:");
+    scanf("%d",&a);
+    if(a=='a'||a=='e'||a=='i'||a=='o'||a=='u')
+    
+        printf("vowel");
+    
+    else
+    
+    
+        printf("costant");
+    
+
+}
